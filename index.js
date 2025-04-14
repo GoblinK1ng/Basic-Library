420d126c9c151601ff22a0523a699ad65cf79396
GoblinK1ng <169468964+GoblinK1ng@users.noreply.github.com>
2025-04-14 21:21:56 +0000

    Added random color to book cards through javascript

--- index.js
+++ index.js
@@ -13,17 +13,22 @@ function addBookToLibrary(title, author, pages){
     myLibrary.push(new Book(title, author, pages));
 }
 
-addBookToLibrary("J","j", 2);
-addBookToLibrary("L","j", 2);
-addBookToLibrary("Z","j", 2);
+addBookToLibrary("Joe the Terrible","Lingus", 300);
+addBookToLibrary("Liza Shmeeza","Bingus", 150);
+addBookToLibrary("Zoey to Zackery","Dingus", 227);
 
 console.table(myLibrary);
 
 function displayArray(){
     for (let i = 0; i < myLibrary.length; i++){
+
         const card = document.createElement("div");
+        
         card.classList.add("card");
-        card.textContent = myLibrary[i].title;
+        card.style.backgroundColor = "rgb("+Math.random()*255+","+Math.random()*255+","+Math.random()*255+")";
+
+
+        card.textContent = myLibrary[i].title + " " + myLibrary[i].author + " " + myLibrary[i].pages;
         mainIndex.appendChild(card);
         console.log("i")
     }
