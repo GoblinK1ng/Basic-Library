97c25ac1f807637f1d08653fb7c2014eeaddbe31
GoblinK1ng <169468964+GoblinK1ng@users.noreply.github.com>
2025-04-14 20:57:01 +0000

    Added to addBookToLibrary function, so a new book can be created and added to Library array

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const myLibrary = [];
 
 function Book(title, author, pages){
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
 }
 
-function addBookToLibrary(){
-
+function addBookToLibrary(title, author, pages){
+    myLibrary.push(new Book(title, author, pages));
 }
+
+addBookToLibrary("J","j", 2);
+console.table(myLibrary);
